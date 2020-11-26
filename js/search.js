07ffb302bf90f1cde7bfc05535cd47dc7c46a1ca
Gloria Requena <grequena@redhat.com>
2020-11-26 17:06:30 -0500

    new layout and search bar

--- js/search.js
+++ js/search.js
@@ -7,11 +7,11 @@
 
       for (var i = 0; i < results.length; i++) {  // Iterate over the results
         var item = store[results[i].ref];
-        appendString += '<li><a href="' + item.url + '"><h3>' + item.title + '</h3></a>';
+        appendString += '<li><a"' + item.url + '"><h3>' + item.title + '</h3></a>';
         appendString += '<p>' + item.content.substring(0, 150) + '...</p></li>';
       }
 
-      searchResults.innerHTML = '<li>"' + item.url + '"</li>';
+      searchResults.innerHTML = appendString;
     } else {
       searchResults.innerHTML = '<li>No results found</li>';
     }
